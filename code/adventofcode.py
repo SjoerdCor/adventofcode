1db47d6d09d353c51eb7e8b532e326d78a96fd57
SjoerdCor <sjoerdcornelissen@gmail.com>
2024-12-28 15:12:56 +0100

    Improve day 6

--- code/adventofcode.py
+++ code/adventofcode.py
@@ -34,3 +34,55 @@ def read(location, to_array=False):
     if len(sections) > 1:
         return sections
     return data
+
+
+def is_outside_bounds(position: tuple, size) -> bool:
+    """Checks whether the position is inside a shape of this size
+
+    Raises ValueError if length of position and size do not match
+    """
+    if len(position) != len(size):
+        raise ValueError(f"Position not of same length as size, {position=}, {size=}")
+    for p, s in zip(position, size):
+        if p < 0 or p >= s:
+            return True
+    return False
+
+
+DIRECTIONS = {
+    "N": np.array([-1, 0]),
+    "E": np.array([0, 1]),
+    "S": np.array([1, 0]),
+    "W": np.array([0, -1]),
+}
+
+
+def find_unique_value_coords(arr: np.ndarray, value):
+    """Find the coordinates of a unique value in a NumPy array.
+
+    Parameters
+    ----------
+    arr : np.ndarray
+        The array to search
+    value :
+        the value to locate
+
+    Returns
+    -------
+    tuple
+        Coordinates of the value.
+
+    Raises
+    ------
+    ValueError
+        If the value is not found or occurs multiple times.
+    """
+    coords = np.argwhere(arr == value)
+    if coords.shape[0] != 1:
+        raise ValueError("Value occurs zero or multiple times.")
+    return tuple(coords[0])
+
+
+def display_arr(arr):
+    """Display array for visual investigation"""
+    print("\n".join("".join(row) for row in arr))
